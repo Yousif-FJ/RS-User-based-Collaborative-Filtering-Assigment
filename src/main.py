2b5485a1dc5184f785c31991d7d7471f4f89b57a
Yousef-FJ <47510107+Yousif-FJ@users.noreply.github.com>
2023-10-29 14:07:16 +0200

    Reshpae the data

--- src/main.py
+++ src/main.py
@@ -7,16 +7,21 @@ path = os.path.join(os.getcwd(),"ratings.csv")
 
 data = pd.read_csv(path)
 
-# print(data.shape)
+print('Showing the top rows')
+print(data.head())
+print()
 
 
-# option = input("""
-#        Select your options:
-#        a)Display first raw and count of rating
-#        """)
+print( f'Number of ratings in the dataset {data.shape[0]}')
 
-transformedData = data.groupby("userId").apply(lambda x: dict(zip(x['movieId'], x['rating'])))
-print(transformedData)
+
+#removing the timestamp because it is unnecessary
+data = data[['userId','movieId','rating']]
+
+#reshape the data to be group by userId and create column for each movieId with rating in it
+reshapedData = data.pivot(index='userId', columns='movieId', values='rating')
+
+print(reshapedData.head())
 
 
 
